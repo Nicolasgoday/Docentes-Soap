f2a70381fb9d5be737142ca3a0ad80ff1ab42813
Nicolasgoday <godaycn87@outlook.com>
2020-10-26 00:46:40 -0300

    add ´prt enviroment

--- src/main/java/com/baeldung/jaxws/client/EmployeeServicePublisher.java
+++ src/main/java/com/baeldung/jaxws/client/EmployeeServicePublisher.java
@@ -4,8 +4,12 @@ import javax.xml.ws.Endpoint;
 
 public class EmployeeServicePublisher {
     public static void main(String[] args) {
-        
-        Endpoint.publish("http://0.0.0.0:8080/employeeservice", 
+    	 String port = System.getenv("PORT");
+         String host = "http://0.0.0.0:";
+         String uri = "/employeeservice";
+           //"http://0.0.0.0:8080/employeeservice", Endpoint.publish(, service);
+        //Endpoint.publish("http://0.0.0.0:8080/employeeservice",
+         Endpoint.publish(host + port + uri ,
           new EmployeeServiceImpl());
     }
 }
