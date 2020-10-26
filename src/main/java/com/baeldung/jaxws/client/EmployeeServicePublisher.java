98a99fa936752315b4fa5d1e69c0876e503a3894
Nicolasgoday <godaycn87@outlook.com>
2020-10-25 23:01:33 -0300

    change port to 80 from 8080

--- src/main/java/com/baeldung/jaxws/client/EmployeeServicePublisher.java
+++ src/main/java/com/baeldung/jaxws/client/EmployeeServicePublisher.java
@@ -5,7 +5,7 @@ import javax.xml.ws.Endpoint;
 public class EmployeeServicePublisher {
     public static void main(String[] args) {
         
-        Endpoint.publish("http://localhost:8080/employeeservice", 
+        Endpoint.publish("http://localhost/employeeservice", 
           new EmployeeServiceImpl());
     }
 }
